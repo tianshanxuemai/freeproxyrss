d28e250f73b02e1af082864d9123c58fcae78d2e
Rokate2021 <77772464+Rokate@users.noreply.github.com>
2021-01-23 20:41:02 +0800

    1.0
    1.0

---
+++ getfree.py
@@ -0,0 +1,19 @@
+import time
+import requests
+today = time.strftime("%m%d")
+
+url = f'https://raw.githubusercontent.com/pojiezhiyuanjun/freev2/master/{today}clash.yml'
+info = requests.get(url)
+if info.status_code == 200:
+    with open('r01.yml', 'w', encoding = 'UTF-8') as f:
+        f.write(info.text)
+else:
+    print (f'{today} no data')
+
+url = f'https://raw.githubusercontent.com/ssrsub/ssr/master/Clash.yml'
+info = requests.get(url)
+if info.status_code == 200:
+    with open('r02.yml', 'w', encoding = 'UTF-8') as f:
+        f.write(info.text)
+else:
+    print (f'{today} no data')
