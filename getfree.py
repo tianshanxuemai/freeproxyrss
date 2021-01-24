f7814d59cec61f6dac879c56be1fb66246ba09e1
Rokate2021 <77772464+Rokate@users.noreply.github.com>
2021-01-24 13:39:56 +0800

    fix
    fix

--- getfree.py
+++ getfree.py
@@ -3,25 +3,25 @@ import requests
 today = time.strftime("%m%d")
 
 url = f'https://raw.githubusercontent.com/pojiezhiyuanjun/freev2/master/{today}clash.yml'
-info = requests.get(url)
+info1 = requests.get(url)
 if info.status_code == 200:
-    with open('r01.yml', 'w', encoding = 'UTF-8') as f:
-        f.write(info.text)
+    with open('free01.yml', 'w', encoding = 'UTF-8') as f:
+        f.write(info1.text)
 else:
     print (f'{today} no data')
 
 url = f'https://raw.githubusercontent.com/ssrsub/ssr/master/Clash.yml'
-info = requests.get(url)
+info2 = requests.get(url)
 if info.status_code == 200:
-    with open('r02.yml', 'w', encoding = 'UTF-8') as f:
-        f.write(info.text)
+    with open('free02.yml', 'w', encoding = 'UTF-8') as f:
+        f.write(info2.text)
 else:
     print (f'{today} no data')
 
 url = f'https://proxypoolss.tk/clash/proxies'
-info = requests.get(url)
+info3 = requests.get(url)
 if info.status_code == 200:
-    with open('r03.yml', 'w', encoding = 'UTF-8') as f:
-        f.write(info.text)
+    with open('free03.yml', 'w', encoding = 'UTF-8') as f:
+        f.write(info3.text)
 else:
     print (f'{today} no data')
